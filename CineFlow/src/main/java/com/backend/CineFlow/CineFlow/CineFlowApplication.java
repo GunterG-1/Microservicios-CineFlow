a9a163c58b5fadad0f1d7a27693ada49363b6da0
Gunter <Gu.garate@duocuc.cl>
2026-05-12 11:49:30 -0400

    Microservicios y api(no completado)

---
+++ CineFlow/src/main/java/com/backend/CineFlow/CineFlow/CineFlowApplication.java
@@ -0,0 +1,15 @@
+package com.backend.CineFlow.CineFlow;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+
+public class CineFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineFlowApplication.class, args);
+	}
+
+}
